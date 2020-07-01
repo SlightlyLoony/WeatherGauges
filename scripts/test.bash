cce2803c45cb2bd5f782163f72708b6542755d7e
Tom Dilatush <tom@dilatush.com>
2020-07-01 09:41:58 -0600

    Deploy bash file, WIP...

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,14 +1,23 @@
-test() {
-  echo $#
-  echo "${1}"
+_ssh() {
+  local SSH
+  read -r -d '' SSH
+  ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$1"@"$2" "$SSH" 2>/dev/null
 }
 
-read -r -d '' VAR <<'EOF'
-this
-that
 
+IMAGE_ID=$(_ssh pi weathergauges << EOF
+  cat /ImageID
 EOF
-
-echo "${VAR}"
-test "${VAR}"
-
+)
+EC=$?
+if (( EC == 0 ))
+then
+  if [[ $IMAGE_ID == "Weather Gauges 1" ]]
+  then
+    echo "We're good!"
+  else
+    echo "Oh, oh - we don't know this image ID: ${IMAGE_ID}"
+  fi
+else
+  echo "Bad things just happened, SSH returned exit code ${EC} when attempting to get image ID"
+fi
